b55e3bd93115516619f81ad43936ba65e1936931
George Wong <george@somewhereville.com>
2016-01-24 18:22:00 -0500

    Clean up directory and add merge.c file

---
+++ merge.c
@@ -0,0 +1,16 @@
+//
+// Simple program used to demonstrate a git-merge
+//
+// For NYU CS480 lab 0
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("a\n");
+  printf("B\n");
+  printf("3\n");
+
+}
+
